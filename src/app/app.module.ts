e820a631a0d48e4a61a16e8b218bb9fe96d51952
Adepu Naresh <Adepu.Naresh-ext@renaissance.com>
2021-06-01 21:41:40 +0530

    Add existing file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { DialogBoxComponent } from './dialog-box/dialog-box.component';
+import { UserTableComponent } from './user-table/user-table.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, DialogBoxComponent ],
+  declarations: [ AppComponent, HelloComponent, DialogBoxComponent, UserTableComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
